2ff785c60688aadd2dcd4aadc5730f9d878ef061
Claran Martis <claranjmartis@gmail.com>
2019-02-22 23:37:51 -0500

    Seaborn plots added to the code

--- city_users_info.py
+++ city_users_info.py
@@ -8,10 +8,16 @@ Created on Sat Feb 16 22:21:46 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+data_full = pd.read_csv('CheckedIn.csv')
 data = pd.read_csv('LIRR.csv')
+data = data[:10]
 station_list = pd.read_csv('stops.csv')
 
+
+#function to convert the station code from a string of numbers
+#mixed with different characters to a list of station codes as integers
 def station_no(a):
     lst = []
     for i in a:
@@ -29,18 +35,17 @@ def station_no(a):
             x = 0
     return l
 
-def generate_key(a,b):
-    x = station_list['stop_name'].loc[station_list['stop_id'] == a]
-    y = station_list['stop_name'].loc[station_list['stop_id'] == b]
-    key = x.values + y.values
-    return key
-    
-    
 
-count = data.groupby('agencyId').count()
-S = pd.Series(count['installationId'])
-S.to_csv('city_users_count.csv')
-
+    
+#counts the numbers of app users taking this service
+user_count = data_full.groupby('agencyId').count()
+A = pd.Series(user_count['installationId'])
+A.to_csv('city_users_count.csv')
+A_head = A.nlargest(10)
+sns.barplot(A_head.index, A_head.values)
+plt.xticks(rotation=-45)
+plt.title('Users taking differnt services')
+plt.show()
 
 count = {}
 stops_col = data['tripStopIds']
@@ -49,29 +54,42 @@ stops_col = data['tripStopIds']
 for row in stops_col:
     row = station_no(row)
     for stop in row:
-        if stop in count:
-            count[stop] = count[stop] + 1
-        else:
-            count[stop] = 1
+        stop = station_list['stop_name'].loc[station_list['stop_id'] == stop]
+        try:
+            stp = str(stop.values[0])
+            if stp in count:
+                count[stp] = count[stp] + 1
+            else:
+                count[stp] = 1
+        except:
+            pass
 
 #plt.bar(count.keys(), count.values())
 #plt.show()
 
 S = pd.Series(count)
+S_head = S.nlargest(10)
+sns.barplot(list(S_head.index), list(S_head.values))
+plt.title('Train traffic at the Station')
+plt.xticks(rotation=-45)
+plt.show()
+
 S.to_csv('station_traffic.csv')
+
 
 route = {}
 for row in stops_col:
     row = station_no(row)
     prev_stop = row[0]
     for stop in row:
+        if(prev_stop == stop):
+            continue
         if(prev_stop > stop):
             temp = stop
             stop = prev_stop
             prev_stop = temp
         x = station_list['stop_name'].loc[station_list['stop_id'] == prev_stop]
         y = station_list['stop_name'].loc[station_list['stop_id'] == stop]
-        
         try:
             key = str(x.values[0]) + str(" - ") + str(y.values[0])
             
@@ -95,8 +113,12 @@ for row in stops_col:
 
 
 s = pd.Series(route)
+s_head = s.nlargest(10)
+sns.barplot(list(s_head.index), list(s_head.values))
+plt.title('Train traffic in different routes')
+plt.xticks(rotation=-45)
+plt.show()
 s.to_csv('route_traffic_3.csv')
-
 
 
 
